00494bb505c9d2f0103fd7d8a37187403b52d00b
Topang4 <155427519+Topang4@users.noreply.github.com>
2025-09-24 00:12:31 +0300

    Added images, styling and web loading transition

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ window.addEventListener("load", function () {
 setTimeout(function () {
   document.getElementById("animated-about").classList.add("visible");
 }, 800);
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.body.classList.add("loaded");
+  }, 300);
+});
